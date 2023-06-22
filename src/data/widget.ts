5aa214495a40772c6f258867002cb07cdf5d0d20
GeovaniSS <geosilvasousa@gmail.com>
2023-06-21 21:16:05 -0300

    feat: Add widget prompts

---
+++ src/data/widget.ts
@@ -0,0 +1,20 @@
+export const widgetPrompts = [
+  {
+    type: "input",
+    name: "widgetName",
+    message: "What is your widget name?",
+    default: "my-widget",
+  },
+  {
+    type: "confirm",
+    name: "isTypescript",
+    message: "Would you like to use TypeScript in your widget?",
+    default: false,
+  },
+  {
+    type: "confirm",
+    name: "addTools",
+    message: "Do you want to add additional tools?",
+    default: false,
+  },
+];
