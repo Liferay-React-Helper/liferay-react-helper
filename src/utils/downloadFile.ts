cef2a92c9272ade5fd1c3a3be74e080c2e370641
GeovaniSS <geosilvasousa@gmail.com>
2023-06-23 23:21:10 -0300

    feat: Add function to download templates files from github repository

---
+++ src/utils/downloadFile.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import { DownloaderHelper } from "node-downloader-helper";
+
+export const downloadFile = async (
+  resourcePath: string,
+  destinationPath = "",
+): Promise<void> => {
+  const user = process.env.GITHUB_USER;
+  const repo = process.env.GITHUB_REPO;
+  const branch = process.env.GITHUB_BRANCH;
+  const baseUrl = `https://raw.githubusercontent.com/${user}/${repo}/${branch}/templates/`;
+
+  return new Promise((resolve, reject) => {
+    const dl = new DownloaderHelper(
+      `${baseUrl}${resourcePath}`,
+      `${process.cwd()}${destinationPath}`,
+    );
+
+    dl.on("end", () => {
+      console.log("Download Completed");
+      resolve();
+    });
+
+    dl.start().catch((err) => {
+      console.error(err);
+      reject(err);
+    });
+
+    dl.on("error", (err) => {
+      console.log("Download Failed", err);
+      reject(err);
+    });
+  });
+};
