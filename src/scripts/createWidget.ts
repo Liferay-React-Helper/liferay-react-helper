6bcf045bc3c80aabc36dbc0f984115845536b1d3
GeovaniSS <geosilvasousa@gmail.com>
2023-06-25 11:49:45 -0300

    feat: Adapt createWidget script to create react/js and react/ts widgets

--- src/scripts/createWidget.ts
+++ src/scripts/createWidget.ts
@@ -8,7 +8,8 @@ export const createWidget = async ({
   widgetName,
   isTypescript,
 }: WidgetDetails) => {
-  const folder = isTypescript ? "react/ts" : "react/js";
+  const folder = isTypescript ? "ts" : "js";
+  const extension = isTypescript ? "tsx" : "js";
 
   silentExec(`liferay new ${widgetName} --batch`);
 
@@ -19,17 +20,23 @@ export const createWidget = async ({
   mkdir("src/css");
 
   await Promise.all([
-    downloadFile(`${folder}/.npmbundlerrc`),
-    downloadFile(`${folder}/.babelrc`),
-    downloadFile(`${folder}/src/index.tsx`, "/src"),
-    downloadFile(`${folder}/src/liferay.d.ts`, "/src"),
-    downloadFile(`${folder}/src/App.tsx`, "/src"),
-    downloadFile(`${folder}/src/css/styles.scss`, "/src/css"),
-    isTypescript ? downloadFile(`${folder}/tsconfig.json`) : null,
+    downloadFile(`react/styles.scss`, "/src/css"),
+    downloadFile(`react/${folder}/.babelrc`),
+    downloadFile(`react/${folder}/src/index.${extension}`, "/src"),
+    downloadFile(`react/${folder}/src/App.${extension}`, "/src"),
+    isTypescript ? downloadFile(`react/${folder}/.npmbundlerrc`) : null,
+    isTypescript
+      ? downloadFile(`react/${folder}/src/liferay.d.ts`, "/src")
+      : null,
+    isTypescript ? downloadFile(`react/${folder}/tsconfig.json`) : null,
   ]);
 
-  const typescriptDevDependencies = isTypescript
+  const tsDevDependencies = isTypescript
     ? [
+        "@babel/core",
+        "@babel/cli",
+        "@babel/preset-env",
+        "@babel/preset-react",
         "@babel/preset-typescript",
         "typescript",
         "@types/react",
@@ -37,37 +44,35 @@ export const createWidget = async ({
       ]
     : [];
 
+  const tsScripts = isTypescript
+    ? [
+        {
+          key: "check-types",
+          value: "tsc",
+        },
+        {
+          key: "configure",
+          value: "liferay deploy --configure",
+        },
+        {
+          key: "build",
+          value:
+            'npm run check-types && babel --source-maps -d build src --extensions ".ts,.tsx" && liferay-npm-bundler',
+        },
+        {
+          key: "deploy",
+          value: "npm run build && liferay deploy --only",
+        },
+        {
+          key: "clean",
+          value: "liferay clean",
+        },
+      ]
+    : [];
+
   return {
     dependencies: ["@liferay/portal-7.4"],
-    devDependencies: [
-      "@babel/core",
-      "@babel/cli",
-      "@babel/preset-env",
-      "@babel/preset-react",
-      ...typescriptDevDependencies,
-    ],
-    scripts: [
-      {
-        key: "check-types",
-        value: "tsc",
-      },
-      {
-        key: "configure",
-        value: "liferay deploy --configure",
-      },
-      {
-        key: "build",
-        value:
-          'npm run check-types && babel --source-maps -d build src --extensions ".ts,.tsx" && liferay-npm-bundler',
-      },
-      {
-        key: "deploy",
-        value: "npm run build && liferay deploy --only",
-      },
-      {
-        key: "clean",
-        value: "liferay clean",
-      },
-    ],
+    devDependencies: [...tsDevDependencies],
+    scripts: [...tsScripts],
   };
 };
